9e301dda05055d1495399acad703b2877f1fda2a
EAM26 <emuller2607@gmail.com>
2025-03-26 13:21:56 +0100

    Add get alarm intakes functionality with Alarms.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home.jsx";
 import AlarmIntakeForm from "./pages/Alarm/alarmIntakeForm/AlarmIntakeForm.jsx";
 import User from "./pages/User/User.jsx";
 import Users from "./pages/User/Users.jsx";
+import Alarms from "./pages/Alarm/alarmIntakeForm/Alarms.jsx";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/alarm" element={<AlarmIntakeForm />} />
-          {/*<Route path="/alarms" element={<Alarms />} />*/}
+          <Route path="/alarms" element={<Alarms />} />
           <Route path="/user" element={<User />} />
           <Route path="/users" element={<Users />} />
       </Routes>
