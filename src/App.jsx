b9f590c07fcc5a0e601e44d96acdac8dc7e0587c
EAM26 <emuller2607@gmail.com>
2025-04-09 10:40:49 +0200

    Update authority access to user pages.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import {Navigate, Route, Routes} from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import AlarmIntakeForm from "./pages/Alarm/alarmIntakeForm/AlarmIntakeForm.jsx";
-import User from "./pages/User/User.jsx";
-import Users from "./pages/User/Users.jsx";
+import User from "./pages/user/User.jsx";
+import Users from "./pages/users/Users.jsx";
 import Alarms from "./pages/Alarm/alarmIntakeForm/Alarms.jsx";
 import UserContextProvider from "./context/UserContext.jsx";
 
@@ -29,8 +29,8 @@ function App() {
                     <Route path="/alarms"
                            element={user && (user.role === 'ADMIN' || user.role === 'MANAGER') ? <Alarms/> :
                                <Navigate to="/login"/>}/>
-                    <Route path="/user" element={user && user.role === 'ADMIN' ? <User/> : <Navigate to="/login"/>}/>
-                    <Route path="/users" element={user && user.role === 'ADMIN' ? <Users/> : <Navigate to="/login"/>}/>
+                    <Route path="/user" element={user && (user.role === 'ADMIN' || user.role === 'MANAGER') ? <User/> : <Navigate to="/login"/>}/>
+                    <Route path="/users" element={user && (user.role === 'ADMIN' || user.role === 'MANAGER') ? <Users/> : <Navigate to="/login"/>}/>
                 </Routes></UserContextProvider>
         </>
     );
